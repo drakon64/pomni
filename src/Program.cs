ed1c6de3f5482fbc5a29f5e974b7263b96a60467
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-06-17 19:53:30 +0100

    Remove most commands

--- src/Program.cs
+++ src/Program.cs
@@ -11,23 +11,8 @@ class Program
         var initCommand = new Command("init");
         rootCommand.Add(initCommand);
         
-        var addCommand = new Command("add");
-        rootCommand.Add(addCommand);
-        
         var updateCommand = new Command("update");
         rootCommand.Add(updateCommand);
-        
-        var modifyCommand = new Command("modify");
-        rootCommand.Add(modifyCommand);
-        
-        var removeCommand = new Command("remove");
-        rootCommand.Add(removeCommand);
-        
-        var freezeCommand = new Command("freeze");
-        rootCommand.Add(freezeCommand);
-        
-        var unfreezeCommand = new Command("unfreeze");
-        rootCommand.Add(unfreezeCommand);
         
         var botCommand = new Command("bot");
         rootCommand.Add(botCommand);
