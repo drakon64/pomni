b57883a241b57ef16348f1d88297cbf8e73a3f1a
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-06-21 03:23:03 +0100

    Shrink Pomni `default.nix`

--- pomni/default.nix
+++ pomni/default.nix
@@ -1,10 +1,7 @@
-let
-  pins = builtins.fromJSON (builtins.readFile ./pomni.lock.json);
-in
 builtins.mapAttrs (
   name: args:
   (builtins.fetchTarball {
     url = args.url;
     sha256 = args.hash;
   })
-) pins
+) (builtins.fromJSON (builtins.readFile ./pomni.lock.json))
